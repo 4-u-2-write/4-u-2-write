6f0803375b3277553b7624076bd916ba01b45a0e
Tazzy2018 <tazemania@hotmail.co.uk>
2021-06-25 15:35:27 -0400

    started html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
 import './App.css';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+
+      <h1>Bootcamp Diaries</h1>
+
+<form>
+  <label>
+    <FontAwesomeIcon className='star' icon={faStar} />
+      <input type="text" placeholder="Tell me about your day" />
+    </label>
+  </form>
+
+
+<div className="text-box">
+  <textarea id="story" name="story"placeholder="Start writing here...."
+          rows="25" cols="75">
+  </textarea>
+</div>    
+
+
+
+
+
+      
     </div>
   );
 }
