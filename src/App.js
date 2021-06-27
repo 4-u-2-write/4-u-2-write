aaf53901a4509baf89d035bc9d29a03e658fb0e7
Tazzy2018 <tazemania@hotmail.co.uk>
2021-06-27 11:39:06 -0400

    added Prompts component

--- src/App.js
+++ src/App.js
@@ -2,37 +2,38 @@
 import Timer from './Timer';
 import { TimeForm } from './Timer';
 import './App.css';
-import { faClock, faFile, faSave, faStar, faSun } from '@fortawesome/free-regular-svg-icons';
+import { faClock, faEdit, faFile, faSave, faSun } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faPen, faPenAlt } from '@fortawesome/free-solid-svg-icons';
 import firebase from './firebase';
 import { useEffect, useState } from 'react';
+import Prompts from './Prompts';
 
 
 
 function App() {
-  const [prompts, setPrompts] = useState([]);
-  const [userPromptInput, setUserPromptInput] = useState('');
-  const [displayPrompt, setDisplayPrompt] = useState('');
+  // const [prompts, setPrompts] = useState([]);
+  // const [userPromptInput, setUserPromptInput] = useState('');
+  // const [displayPrompt, setDisplayPrompt] = useState('');
   // const [entries, setEntries] = useState('');
   const [userEntryInput, setUserEntryInput] = useState('');
 
   // event listener functions 
   // 1. user typing in text input
-  const handlePromptChange = (e) => {
-    setUserPromptInput(e.target.value);
-  }
+  // const handlePromptChange = (e) => {
+  //   setUserPromptInput(e.target.value);
+  // }
   
-  // 2. click on button
-  const handlePromptClick = (e) => {
-    e.preventDefault();
-  // Check for empty input before adding to database
-    if (userPromptInput !== "") {
-      const dbRefPrompts = firebase.database().ref('/Prompts');
-      dbRefPrompts.push(userPromptInput);
-    }
-    setUserPromptInput('');
-  }
+  // // 2. click on button
+  // const handlePromptClick = (e) => {
+  //   e.preventDefault();
+  // // Check for empty input before adding to database
+  //   if (userPromptInput !== "") {
+  //     const dbRefPrompts = firebase.database().ref('/Prompts');
+  //     dbRefPrompts.push(userPromptInput);
+  //   }
+  //   setUserPromptInput('');
+  // }
 
 
   const handleEntryChange = (e) => {
@@ -50,42 +51,46 @@ function App() {
 
 
 
-  useEffect(() => {
-    // ref to firebase with nested prompts data
-    const dbRefPrompts = firebase.database().ref('/Prompts');
-    const dbRefEntries = firebase.database().ref('/Entries');
+  // useEffect(() => {
+  //   // ref to firebase with nested prompts data
+  //   const dbRefPrompts = firebase.database().ref('/Prompts');
+    // const dbRefEntries = firebase.database().ref('/Entries');
 
-    dbRefPrompts.on('value', (response) => {
-      // console.log(response.val());
-      const newState = [];
+  //   dbRefPrompts.on('value', (response) => {
+  //     // console.log(response.val());
+  //     const newState = [];
 
-      const promptData = response.val();
+  //     const promptData = response.val();
 
-      for (let key in promptData) {
-        newState.push(promptData[key]);
-      }
-      setPrompts(newState);
-      // console.log(newState);
+  //     for (let key in promptData) {
+  //       newState.push(promptData[key]);
+  //     }
+  //     setPrompts(newState);
+  //     // console.log(newState);
     
-    });
+  //   });
 
     
-  }, [])
+  // }, [])
   
-  const randomizer = () => {
-    // variable with random index number, based on array length
-    const randomIndex = Math.floor(Math.random() * prompts.length);
-    setDisplayPrompt(prompts[randomIndex]);
-    return displayPrompt;
-    }
+  // const randomizer = () => {
+  //   // variable with random index number, based on array length
+  //   const randomIndex = Math.floor(Math.random() * prompts.length);
+  //   setDisplayPrompt(prompts[randomIndex]);
+  //   return displayPrompt;
+  //   }
 
   return (
     <div className="App">
 
       <h1>Bootcamp Diaries</h1>
 
+      <div> 
+        <Prompts />
+      </div>
 
-      <form>
+
+      {/* <form>
         <label htmlFor="newPrompt">
           <FontAwesomeIcon className='star' icon ={faStar} />
             <input 
@@ -101,13 +106,13 @@ function App() {
         <button type="submit" onClick={randomizer}>Ask for a prompt</button>
         <p> 
           {displayPrompt}
-        </p>
+        </p> */}
 
   <div className="icons">
+
     <FontAwesomeIcon className='clock' size='2x' icon ={faClock} />
 
-  
-    <FontAwesomeIcon className='pen' size='2x' icon ={faPen} />
+    <FontAwesomeIcon className='pen' size='2x' icon ={faEdit} />
 
     <FontAwesomeIcon className='page' size='2x' icon ={faFile} />
 
