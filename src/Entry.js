b18c5d03dac248ac7f5601476ad3f4f88063460a
Tiffany Fust <tiffanyfust@gmail.com>
2021-06-29 17:16:27 -0700

    removed entry form button, Entry pushes to firebase on Save Icon click

--- src/Entry.js
+++ src/Entry.js
@@ -1,49 +1,48 @@
-import firebase from "./firebase";
-import { useState, useEffect } from "react";
+// import firebase from "./firebase";
+// import { useState, useEffect } from "react";
 
-const Entry = () => {
+// const Entry = () => {
 
-  const [userEntryInput, setUserEntryInput] = useState('');
+  // const [userEntryInput, setUserEntryInput] = useState('');
 
-  const handleEntryChange = (e) => {
-    setUserEntryInput(e.target.value);
-    if (userEntryInput !== "") {
-      
-    }
-  }
+  // const handleEntryChange = (e) => {
+  //   setUserEntryInput(e.target.value);
+  // }
+
 // if text box value stops changing ( aka user stops typing) for 15 seconds then show alert
-  useEffect(() => {
-    const timeoutId = setInterval(() => alert("Get back to work!"), 15000);
-    return () => clearTimeout(timeoutId);
-  }, [userEntryInput]);
+  // useEffect(() => {
+  //   const timeout = setInterval(() => alert("Get back to work!"), 5000);
+  //   return () => clearInterval(timeout);
+  // }, [userEntryInput]);
+  
 
-  // when save button is clicked push user entry to firebase database
-  const handleEntryClick = (e) => {
-    e.preventDefault();
-    if (userEntryInput !== "") {
-      const dbRefEntries = firebase.database().ref('/Entries');
-      dbRefEntries.push(userEntryInput);
-    }
-    setUserEntryInput('');
-  }
+  // // when save button is clicked push user entry to firebase database
+  // const handleEntryClick = (e) => {
+  //   e.preventDefault();
+  //   if (userEntryInput !== "") {
+  //     const dbRefEntries = firebase.database().ref('/Entries');
+  //     dbRefEntries.push(userEntryInput);
+  //   }
+  //   setUserEntryInput('');
+  // }
 
 
-  return (
-    <>
-      <form className="text-box">
-        <textarea
-          id="story" name="story"
-          placeholder="Start writing here...."
-          rows="25" cols="75"
-          onChange={handleEntryChange}
-          value={userEntryInput}
+//   return (
+//     // <>
+//     //   <form className="text-box">
+//     //     <textarea
+//     //       id="story" name="story"
+//     //       placeholder="Start writing here...."
+//     //       rows="25" cols="75"
+//     //       onChange={handleEntryChange}
+//     //       value={userEntryInput}
 
-        >
-        </textarea>
-        <button type="submit" onClick={handleEntryClick} >Save Entry</button>
-      </form>
-    </>
-  )
-}
+//     //     >
+//     //     </textarea>
+//     //     {/* <button type="submit">Save Entry</button> */}
+//     //   </form>
+//     // </>
+//   )
+// }
 
-export default Entry;
+// export default Entry;
